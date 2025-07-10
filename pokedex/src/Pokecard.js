e50043c6b85d7f2db0d3f64c557461a6f30a1891
L3ofspades <jonathanfederico13@gmail.com>
2025-07-09 22:28:19 -0700

    trying to get the picture to pop up on page

--- pokedex/src/Pokecard.js
+++ pokedex/src/Pokecard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 
 function Pokecard({ id, name, type, base_experience }) {
@@ -6,7 +6,10 @@ function Pokecard({ id, name, type, base_experience }) {
     return (
         <div className="Pokecard" style={{ border: "1px solid #ccc", padding: "10px", margin: "10px", textAlign: "center" }}>
         <h3>{name}</h3>
-        <img src={imgSrc} alt={name} />
+        <img src={imgSrc} alt={name} onError={(e) =>{
+            e.target.onerror = null;
+            e.target.src = "https://via.placeholder.com/96?text=Image+Not+Found";
+        }}/>
         <p>Type: {type}</p>
         <p>Exp: {base_experience}</p>
         </div>
